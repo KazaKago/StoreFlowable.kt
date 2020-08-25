da44e92a260a15542f6b14f2b0c1b6afe1888520
Kensuke Tamura <weathercock_perch@yahoo.co.jp>
2020-08-26 00:43:22 +0900

    update libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
-        classpath(kotlin("gradle-plugin", "1.3.72"))
+        classpath(kotlin("gradle-plugin", "1.4.0"))
         classpath("com.novoda:bintray-release:1.0.3")
 
         // NOTE: Do not place your application dependencies here; they belong
