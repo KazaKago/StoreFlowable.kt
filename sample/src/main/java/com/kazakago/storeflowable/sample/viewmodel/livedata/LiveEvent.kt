c7be31897745cb4c0b023d50da325b9034af2aaf
Kensuke Tamura <kazakago@gmail.com>
2020-11-28 02:49:03 +0900

    fix sample.

--- sample/src/main/java/com/kazakago/storeflowable/sample/viewmodel/livedata/LiveEvent.kt
+++ sample/src/main/java/com/kazakago/storeflowable/sample/viewmodel/livedata/LiveEvent.kt
@@ -40,13 +40,13 @@ open class LiveEvent<T> : LiveData<T>() {
 
     @MainThread
     open fun observe(owner: LifecycleOwner, tag: String, observer: Observer<in T>) {
-        super.observe(owner, Observer<T> {
+        super.observe(owner) {
             val internalTag = owner::class.java.name + "#" + tag
             if (!dispatchedTagSet.contains(internalTag)) {
                 dispatchedTagSet.add(internalTag)
                 observer.onChanged(it)
             }
-        })
+        }
     }
 
     @MainThread
