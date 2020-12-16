7660e6910f90dc7a9737fe0ff6a903f3a04586ef
Kensuke Tamura <kazakago@gmail.com>
2020-12-16 12:09:28 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val versionName by extra("1.0.1")
+    val versionName by extra("2.0.0-SNAPSHOT")
     repositories {
         google()
         jcenter()
