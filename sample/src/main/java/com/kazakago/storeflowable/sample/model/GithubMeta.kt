249bf10c4e4cf9d60ceb5a1fc8cdda54b8288e7f
Kensuke Tamura <kazakago@gmail.com>
2020-12-16 15:28:21 +0900

    fix github api changes

--- sample/src/main/java/com/kazakago/storeflowable/sample/model/GithubMeta.kt
+++ sample/src/main/java/com/kazakago/storeflowable/sample/model/GithubMeta.kt
@@ -12,10 +12,6 @@ data class GithubMeta(
 ) {
     @JsonClass(generateAdapter = true)
     data class SshKeyFingerprints(
-        @Json(name = "MD5_RSA")
-        val md5Rsa: String,
-        @Json(name = "MD5_DSA")
-        val md5Dsa: String,
         @Json(name = "SHA256_RSA")
         val sha256Rsa: String,
         @Json(name = "SHA256_DSA")
