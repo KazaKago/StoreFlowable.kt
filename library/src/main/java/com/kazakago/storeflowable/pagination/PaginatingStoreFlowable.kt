a7f129e4c1af9ad74f05b958ec98ed412065a3ef
Kensuke Tamura <kazakago@gmail.com>
2021-04-14 23:05:01 +0900

    remove StoreFlowable implemention from PaginatingStoreFlowable

--- library/src/main/java/com/kazakago/storeflowable/pagination/PaginatingStoreFlowable.kt
+++ library/src/main/java/com/kazakago/storeflowable/pagination/PaginatingStoreFlowable.kt
@@ -1,8 +1,21 @@
 package com.kazakago.storeflowable.pagination
 
-import com.kazakago.storeflowable.StoreFlowable
+import com.kazakago.storeflowable.AsDataType
+import com.kazakago.storeflowable.core.FlowableState
 
-interface PaginatingStoreFlowable<KEY, DATA> : StoreFlowable<KEY, DATA> {
+interface PaginatingStoreFlowable<KEY, DATA> {
+
+    fun publish(forceRefresh: Boolean = false): FlowableState<DATA>
+
+    suspend fun getData(type: AsDataType = AsDataType.Mix): DATA?
+
+    suspend fun requireData(type: AsDataType = AsDataType.Mix): DATA
+
+    suspend fun validate()
+
+    suspend fun refresh(clearCacheWhenFetchFails: Boolean = true, continueWhenError: Boolean = true)
 
     suspend fun requestAddition(continueWhenError: Boolean = true)
+
+    suspend fun update(newData: DATA?)
 }
