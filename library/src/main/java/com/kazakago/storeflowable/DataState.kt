941e761934dbf82865bd3df3bfb26f8219a2c79e
Kensuke Tamura <kazakago@gmail.com>
2021-06-13 17:04:52 +0900

    change sealed interface from class

--- library/src/main/java/com/kazakago/storeflowable/DataState.kt
+++ library/src/main/java/com/kazakago/storeflowable/DataState.kt
@@ -5,24 +5,24 @@ package com.kazakago.storeflowable
  *
  * This state is only used inside the [StoreFlowable].
  */
-sealed class DataState {
+sealed interface DataState {
 
     /**
      * No processing state.
      *
      * @property noMoreAdditionalData Set to `true` if you know at Pagination that there is no more additional data. Has no effect without Pagination.
      */
-    class Fixed(val noMoreAdditionalData: Boolean = false) : DataState()
+    class Fixed(val noMoreAdditionalData: Boolean = false) : DataState
 
     /**
      * Acquiring data state.
      */
-    class Loading : DataState()
+    class Loading : DataState
 
     /**
      * An error when processing state.
      *
      * @property exception The entity of the exception that occurred.
      */
-    class Error(val exception: Exception) : DataState()
+    class Error(val exception: Exception) : DataState
 }
