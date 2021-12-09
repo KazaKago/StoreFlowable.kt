add6a4822580740b65cbd4ef4c52fafc5a6d0805
Kensuke Tamura <kazakago@gmail.com>
2021-12-09 11:39:49 +0900

    add -SNAPSHOT to version name.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val versionName by extra("5.2.0")
+    val versionName by extra("5.3.0-SNAPSHOT")
     repositories {
         google()
         mavenCentral()
