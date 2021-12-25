cc43cbf9da5dbffdf9296f94cf9d52aec298077f
Kensuke Tamura <kazakago@gmail.com>
2021-12-25 11:50:48 +0900

    update libraries.

--- storeflowable/build.gradle.kts
+++ storeflowable/build.gradle.kts
@@ -40,13 +40,13 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 api(project(":storeflowable-core"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0-RC3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
                 implementation("io.kotest:kotest-assertions-core:5.0.2")
             }
         }
