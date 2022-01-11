fd9ff35f4081d99a2184d7f18859caedaa3fe5c1
Kensuke Tamura <kazakago@gmail.com>
2022-01-11 19:28:30 +0900

    fix publishing

--- buildSrc/src/main/kotlin/SetupPublishing.kt
+++ buildSrc/src/main/kotlin/SetupPublishing.kt
@@ -1,23 +1,17 @@
-import org.gradle.api.Action
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.*
 import org.gradle.plugins.signing.SigningExtension
 
-private fun Project.publishing(configure: PublishingExtension.() -> Unit): Unit =
-    (this as ExtensionAware).extensions.configure("publishing", configure)
+private fun Project.publishing(configure: PublishingExtension.() -> Unit) = configure(configure)
 
-private fun Project.signing(configure: Action<SigningExtension>): Unit =
-    (this as ExtensionAware).extensions.configure("signing", configure)
+private fun Project.signing(configure: SigningExtension.() -> Unit) = configure(configure)
 
 fun Project.setupPublishing(
     version: String = PublishingInfo.versionName,
     groupId: String = PublishingInfo.groupId,
-    artifactId: String = name,
     projectName: String = PublishingInfo.projectName,
     projectDescription: String = PublishingInfo.projectDescription,
     licenseName: String = PublishingInfo.licenseName,
@@ -26,52 +20,47 @@ fun Project.setupPublishing(
     scmConnection: String = PublishingInfo.scmConnection,
     developerName: String = PublishingInfo.developerName,
     developerEmail: String = PublishingInfo.developerEmail,
-    developerUrl: String = PublishingInfo.developerUrl
+    developerUrl: String = PublishingInfo.developerUrl,
 ) {
     this.version = version
     this.group = groupId
-    tasks.create("javadocJar", Jar::class) {
+    val javadocJar by tasks.registering(Jar::class) {
         group = "publishing"
         archiveClassifier.set("javadoc")
         from(tasks["dokkaHtml"])
     }
     publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                artifact(tasks["javadocJar"])
-                this.version = version
-                this.groupId = groupId
-                this.artifactId = artifactId
-                pom {
-                    name.set(projectName)
-                    description.set(projectDescription)
+        publications.withType<MavenPublication>().all {
+            artifact(javadocJar.get())
+            pom {
+                name.set(projectName)
+                description.set(projectDescription)
+                url.set(projectUrl)
+                licenses {
+                    license {
+                        name.set(licenseName)
+                        url.set(licenseUrl)
+                    }
+                }
+                scm {
+                    connection.set(scmConnection)
+                    developerConnection.set(scmConnection)
                     url.set(projectUrl)
-                    licenses {
-                        license {
-                            name.set(licenseName)
-                            url.set(licenseUrl)
-                        }
-                    }
-                    scm {
-                        connection.set(scmConnection)
-                        developerConnection.set(scmConnection)
-                        url.set(projectUrl)
-                    }
-                    developers {
-                        developer {
-                            name.set(developerName)
-                            email.set(developerEmail)
-                            url.set(developerUrl)
-                        }
+                }
+                developers {
+                    developer {
+                        name.set(developerName)
+                        email.set(developerEmail)
+                        url.set(developerUrl)
                     }
                 }
-                signing {
-                    val keyId = System.getenv("SIGNING_KEY_ID") ?: properties["signing.keyId"].toString()
-                    val secretKey = System.getenv("SIGNING_SECRET_KEY") ?: properties["signing.secretKey"].toString()
-                    val password = System.getenv("SIGNING_PASSWORD") ?: properties["signing.password"].toString()
-                    useInMemoryPgpKeys(keyId, secretKey, password)
-                    sign(this@create)
-                }
+            }
+            signing {
+                val keyId = System.getenv("SIGNING_KEY_ID") ?: properties["signing.keyId"]?.toString()
+                val secretKey = System.getenv("SIGNING_SECRET_KEY") ?: properties["signing.secretKey"]?.toString()
+                val password = System.getenv("SIGNING_PASSWORD") ?: properties["signing.password"]?.toString()
+                useInMemoryPgpKeys(keyId, secretKey, password)
+                sign(this@all)
             }
         }
         repositories {
@@ -80,8 +69,8 @@ fun Project.setupPublishing(
                 val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
                 url = if (version.endsWith("-SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
                 credentials {
-                    username = System.getenv("SONATYPE_USERNAME") ?: properties["sonatype.username"].toString()
-                    password = System.getenv("SONATYPE_PASSWORD") ?: properties["sonatype.password"].toString()
+                    username = System.getenv("SONATYPE_USERNAME") ?: properties["sonatype.username"]?.toString()
+                    password = System.getenv("SONATYPE_PASSWORD") ?: properties["sonatype.password"]?.toString()
                 }
             }
         }
