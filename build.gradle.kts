cc1c413bd4c915ab216ebc6982cab74040409ab2
Kensuke Tamura <kazakago@gmail.com>
2022-06-15 18:31:32 +0900

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.0" apply false
     id("org.jetbrains.dokka") version "1.6.21" apply false
 }
 
